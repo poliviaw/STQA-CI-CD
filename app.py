7abc7909ed9c1f3c69829b34c9607e51d2ccae28
poliviaw <olivia.putri.winata13@gmail.com>
2025-12-15 05:02:28 +0700

    fix: app.py

--- app.py
+++ app.py
@@ -43,7 +43,10 @@ def handle_pets():
     if request.method == 'GET':
         category = request.args.get('category')
         if category:
-            matching_pets = [pet for pet in pets.values() if pet['category'] == category]
+            matching_pets = [
+                pet for pet in pets.values() 
+                if pet['category'].lower() == category.lower()
+            ]
             return jsonify(matching_pets)
         
         return jsonify(list(pets.values()))
